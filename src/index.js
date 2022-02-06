4b0cc6d499eb38ecb53fa03b0012b7ff4cd54281
Rick1196 <ricardompp11@gmail.com>
2022-02-05 18:31:44 -0600

    Add pwa

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import history from './pages/history';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
